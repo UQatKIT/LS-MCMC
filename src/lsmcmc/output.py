52648abb8375ae4c7e73bc8b1143a2243ef71ebe
Felix Racz <racz@posteo.de>
2025-06-20 11:32:27 +0200

    feat: Added support for simpler instantiation of outputs.

--- src/lsmcmc/output.py
+++ src/lsmcmc/output.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from numbers import Number
+from collections.abc import Iterable
+from typing import Optional, ClassVar
 
 import numpy as np
 
@@ -8,9 +10,16 @@ import numpy as np
 class MCMCQoI(ABC):
     @abstractmethod
     def evaluate(self, state: np.ndarray, accepted: bool) -> Number:
-        """_summary_."""
+        """Evaluates QoI from state vector."""
         raise NotImplementedError
 
+
+    @abstractmethod
+    def name(self) -> str:
+        raise NotImplementedError
+
+    def __str__(self) -> str:
+        return self.name()
 
 # --------------------------------------------------------------------------------------------------
 class ComponentQoI(MCMCQoI):
@@ -20,6 +29,8 @@ class ComponentQoI(MCMCQoI):
     def evaluate(self, state: np.ndarray, _accepted: bool) -> float:
         return state[self._component]
 
+    def name(self) -> str:
+        return f"component {self._component}"
 
 # --------------------------------------------------------------------------------------------------
 class MeanQoI(MCMCQoI):
@@ -27,6 +38,9 @@ class MeanQoI(MCMCQoI):
     def evaluate(state: np.ndarray, _: bool) -> float:
         return np.mean(state)
 
+    @staticmethod
+    def name() -> str:
+        return "mean"
 
 # --------------------------------------------------------------------------------------------------
 class AcceptanceQoI(MCMCQoI):
@@ -34,20 +48,35 @@ class AcceptanceQoI(MCMCQoI):
     def evaluate(_: np.ndarray, accepted: bool) -> float:
         return float(accepted)
 
+    @staticmethod
+    def name() -> str:
+        return "acceptance"
+
 
 # ==================================================================================================
 class MCMCStatistic(ABC):
     @abstractmethod
     def evaluate(self, qoi_value: Number) -> Number:
-        """_summary_."""
+        """Evaluates a certain statistic from a Quantity of Interest."""
         raise NotImplementedError
 
+    @abstractmethod
+    def name(self) -> str:
+        raise NotImplementedError
+
+
+    def __str__(self) -> str:
+        return self.name()
 
 # --------------------------------------------------------------------------------------------------
 class IdentityStatistic(MCMCStatistic):
     @staticmethod
     def evaluate(qoi_value: Number) -> Number:
         return qoi_value
+
+    @staticmethod
+    def name():
+        return "identity"
 
 
 # --------------------------------------------------------------------------------------------------
@@ -64,6 +93,8 @@ class RunningMeanStatistic(MCMCStatistic):
         self._running_value = new_value
         return new_value
 
+    def name(self):
+        return "mean"
 
 # --------------------------------------------------------------------------------------------------
 class BatchMeanStatistic(MCMCStatistic):
@@ -83,6 +114,8 @@ class BatchMeanStatistic(MCMCStatistic):
             self._values.clear()
         return self._batch_mean
 
+    def name(self):
+        return f"BM{self._batch_size}"
 
 # ==================================================================================================
 class MCMCOutput:
@@ -117,3 +150,35 @@ class MCMCOutput:
     @property
     def all_values(self) -> np.ndarray:
         return np.array(self._values)
+
+
+class Acceptance(MCMCOutput):
+    """Output of average acceptance rate."""
+    def __init__(self) -> None:
+        super().__init__(
+            qoi = AcceptanceQoI(),
+            statistic = RunningMeanStatistic(),
+            str_id = f"{'Acceptance':<12}",
+            str_format = "<+12.3e",
+            log = True
+        )
+
+
+class SimplifiedOutput(MCMCOutput):
+
+    def __init__(
+        self,
+        qoi: MCMCQoI,
+        statistic: MCMCStatistic,
+    ) -> None:
+        str_id = f"{statistic} of {qoi}"
+        if isinstance(statistic, IdentityStatistic):
+            str_id = f"{qoi}"
+        str_id = f"{str_id:<12}"
+        super().__init__(
+            qoi = qoi,
+            statistic = statistic,
+            str_id = str_id,
+            str_format = f"<+{max(12, len(str_id))}.3e",
+            log = True
+        )

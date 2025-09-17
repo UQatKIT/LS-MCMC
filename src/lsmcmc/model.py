93a2c44d759f0920ab3f5f9c595e0365b49ec3d9
Felix Racz <racz@posteo.de>
2025-09-17 15:42:06 +0200

    feat: added MALA and docstrings to algorithms

--- src/lsmcmc/model.py
+++ src/lsmcmc/model.py
@@ -19,3 +19,17 @@ class MCMCModel(ABC):
     @abstractmethod
     def reference_point(self) -> np.ndarray[tuple[int], np.dtype[np.floating]]:
         raise NotImplementedError
+
+
+class DifferentiableMCMCModel(MCMCModel):
+    @abstractmethod
+    def evaluate_gradient_of_potential(
+        self, state: np.ndarray[tuple[int], np.dtype[np.floating]]
+    ) -> np.ndarray[tuple[int], np.dtype[np.floating]]:
+        raise NotImplementedError
+    
+    def compute_preconditioner_action(
+        self, state: np.ndarray[tuple[int], np.dtype[np.floating]]
+    ) -> np.ndarray[tuple[int], np.dtype[np.floating]]:
+        sqrt_action =  self.compute_preconditioner_sqrt_action(state)
+        return self.compute_preconditioner_sqrt_action(sqrt_action)
